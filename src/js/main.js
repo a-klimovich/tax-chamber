0d39d6eca15a18b10c02c10c28d059595ce9550c
a-klimovich <ar2r.klimovich@gmail.com>
2021-07-03 12:35:36 +0300

    tablet fixes

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,39 @@ $(document).ready(function(){
     dotsClass: 'slider-dots',
   });
 });
+
+$('#humburgerMenuBtn').on('click', () => {
+  $('#humburgerMenuBtn').toggleClass('active');
+  $('#navHeaderMenu').toggleClass('active');
+})
+
+if ($('#map').length) {
+  ymaps.ready(init);
+
+  function init(){
+    const myLocation = [53.884328, 27.532366];
+
+    myMap = new ymaps.Map("map", {
+      center: myLocation,
+      zoom: 16
+    }),
+
+    myPlacemark = new ymaps.Placemark(myLocation, {}, {
+      preset: 'islands#greenIcon'
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+  }
+}
+
+(function($) {
+  $(function() {
+    
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+    
+  });
+  })(jQuery);
